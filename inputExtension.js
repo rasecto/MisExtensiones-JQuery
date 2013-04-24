555432feb04fbdec817bde7ede023a5934bcaa80
rasecto <cesar_0102_03@hotmail.com>
2013-04-23 20:54:02 -0500

    creacion de extension simple

---
+++ inputExtension.js
@@ -0,0 +1,105 @@
+/* extension simple que permite establecer un texto de comentario
+   en campos de formulario tipo texto  */
+
+
+(function($) {
+
+	/* objeto con funciones utilitarias */
+
+	var _utility = (
+
+	function() {
+
+		return {
+
+			addClasses: function($seleccion, classes) {
+                 
+                 /* restriccion a argumento cadena */
+                 
+				classes = ( $.type(classes) != "string") ? "":classes;
+
+				var vector_clasess = classes.split(" "),
+
+					length = vector_clasess.length;
+
+				for (var i = 0; i < length; i++) {
+
+					$seleccion.addClass(vector_clasess[i]);
+				}
+
+			},
+			removeClasses: function($seleccion, classes) {
+
+				/* restriccion a argumento cadena */
+                 
+				classes = ( $.type(classes) != "string") ? "":classes;
+               
+				var vector_clasess = classes.split(" "),
+
+					length = vector_clasess.length;
+
+				for (var i = 0; i < length; i++) {
+
+					$seleccion.hasClass(vector_clasess[i]) && $seleccion.removeClass(vector_clasess[i]);
+
+				}
+			}
+
+		};
+	}
+
+	)();
+
+
+	$.extend($.fn, {
+
+		showCommentInput: function(comment, focus_class, blur_class) {
+			return this.filter("input:text").val(comment).on({
+
+				focus: function() {
+
+					var $input = $(this),
+
+						text = $.trim($input.val());
+
+					/* if simple anidado en una sola linea */
+
+					(text.length && comment == text) && $input.val("");
+
+					/* clase añadida a $input */
+
+					_utility.addClasses($input, focus_class);
+
+				},
+				blur: function() {
+
+					var $input = $(this),
+
+						texto = $.trim($input.val());
+
+					/* if de una sola linea */
+
+					!texto.length && $input.val(comment);
+
+					/* clase quitada y añadida a $input */
+
+					_utility.removeClasses($input, focus_class);
+					_utility.addClasses($input, blur_class);
+
+					/*  
+                    
+					$input.hasClass(focus_class) && $input.removeClass(focus_class);
+
+					$input.addClass(blur_class);
+                    */
+
+				}
+
+
+			});
+		}
+
+	});
+
+
+})(jQuery);
